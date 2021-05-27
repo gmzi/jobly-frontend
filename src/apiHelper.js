855050bd255a8a719fc1e2b0b417784c0669f122
gmzi <gastonmazieres@gmail.com>
2021-05-27 17:33:28 -0400

    change BASE_URL

--- src/apiHelper.js
+++ src/apiHelper.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+// const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+const BASE_URL =
+  process.env.REACT_APP_BASE_URL || 'https://jobly-1.herokuapp.com';
 
 /** API Class.
  *
